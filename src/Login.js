7d201a76eece06b76df1d7ae6eed2fcc9e84d6f2
MaiCodes-exe <maise.b.costa@gmail.com>
2022-12-12 21:54:58 +0000

    first update

---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_url=https://localhost:3000scope=streaming20%user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"  
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
